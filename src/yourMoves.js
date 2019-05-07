36a962da512d7eeb78d2187fc1bb9e43f2a16cbd
cghenne <cathleen@justbutterit.com>
2019-05-07 19:00:04 -0400

    Block middle column

--- src/yourMoves.js
+++ src/yourMoves.js
@@ -2,8 +2,8 @@ export const x = "❌";
 export const o = "⭕️";
 
 const moves = [
-  [["⭕️"], ["⭕️"], ["❌"]], 
-  [["❌"], [o], []], 
-  [[], [], []]];
+  [[o], [o], [x]], 
+  [[x], [o], []], 
+  [[], [x], []]];
 
 export default moves;
